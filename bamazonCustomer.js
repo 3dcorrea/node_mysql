2e7ac451c584ff094d9f0e7d47e3ffbc428c9ddd
Dan Correa <dancorrea3@gmail.com>
2019-05-11 21:18:26 -0400

    minor changes to for loop console log

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,26 +3,21 @@ var inquirer = require("inquirer");
 var mysql = require("mysql");
 var connection = mysql.createConnection({
     host: "localhost",
-
     port: 3306,
-
     user: "root",
-
     password: ",./jkl890",
     database: "bamazon"
 });
-
 connection.connect(function (err) {
     if (err) throw err;
     runSearch();
 });
-
 function runSearch() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         for (var i = 0; i < res.length; i++) {
-            console.log("Item #: " + res[i].item_id + "|" +
-                "Product: " + res[i].product_name + "|" +
+            console.log("Product ID: " + res[i].item_id + "|" +
+                "Item: " + res[i].product_name + "|" +
                 "Department: " + res[i].department_name + "|" +
                 "Price: " + "$" + res[i].price + "|" +
                 "Inventory: " + res[i].stock_quantity);
@@ -30,9 +25,7 @@ function runSearch() {
         }
         makePurchase();
     });
-
 };
-
 var makePurchase = function () {
     inquirer.prompt([{
         name: "itemId",
@@ -45,18 +38,15 @@ var makePurchase = function () {
     }]).then(function (answer) {
         connection.query("SELECT * FROM products", function (err, res) {
             if (err) throw err;
-
             var product;
             for (var i = 0; i < res.length; i++) {
                 if (res[i].item_id === parseInt(answer.itemId)) {
                     product = res[i];
-
                 }
             }
             for (var i = 0; i < res.length; i++) {
                 if (res[i].item_id === parseInt(answer.itemId)) {
                     product = res[i];
-
                 }
             }
             if (product.stock_quantity > parseInt(answer.quantity)) {
@@ -79,5 +69,4 @@ var makePurchase = function () {
             }
         });
     });
-
 };
